8f2dce415f0ca2b350ddb3b7ccc1815350671a11
Pieupi <upietroguedes@gmail.com>
2026-01-30 21:18:23 -0300

    Add files via upload

---
+++ evobot/interfaces/Config.ts
@@ -0,0 +1,8 @@
+export interface Config {
+  TOKEN: MTQ2NjAxMTMzMjgxMzAwMDc4Ng.GCrFcQ.u0NvyMko5vbuyTj6KHXJYqzNDm4vLuEHLDR4k8;
+  MAX_PLAYLIST_SIZE: 10;
+  PRUNING: false;
+  STAY_TIME: 30;
+  DEFAULT_VOLUME: 100;
+  LOCALE: pt_br;
+}
